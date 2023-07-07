41d19f8c6ed60c39812c140108643303aa9aa8f1
HardiB.Salih <30411248+HardiB-Salih@users.noreply.github.com>
2023-07-08 01:32:06 +0300

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by HardiB.Salih on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
