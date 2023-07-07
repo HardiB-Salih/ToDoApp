a4d849609cbecf687c65f3ce8efd8f98db9af642
HardiB.Salih <30411248+HardiB-Salih@users.noreply.github.com>
2023-07-08 01:34:09 +0300

    Initial Commit

--- ToDoApp/ContentView.swift
+++ ToDoApp/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Welcome Budy")
+            Text("New Change")
         }
         .padding()
     }
